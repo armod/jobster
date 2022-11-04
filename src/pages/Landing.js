27d1cc3ac6088401ff559198e1889e6bd914c2cc
armod <armods@gmail.com>
2022-11-04 21:10:35 +0100

    update

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,7 +5,7 @@ import styled from 'styled-components/macro'
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt='jobster logo' className='logo' />
       </nav>
@@ -15,18 +15,44 @@ const Landing = () => {
           <h1>
             job <span>tracking</span>app
           </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+            ratione alias sequi dignissimos aliquid autem debitis! Consequuntur
+            quibusdam natus qui, numquam aperiam illo possimus magni! Corrupti,
+            repdiandae. Nam, sint mollitia.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
-          ratione alias sequi dignissimos aliquid autem debitis! Consequuntur
-          quibusdam natus qui, numquam aperiam illo possimus magni! Corrupti,
-          repdiandae. Nam, sint mollitia.
-        </p>
-        <button className='btn'>Login/Register</button>
+        <img src={main} alt='some logo' className='img main-img' />
       </div>
-      <img src={main} alt='some logo' />
-    </main>
+    </Wrapper>
   )
 }
 
 export default Landing
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+
+  .page {
+    min-hight: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+`
