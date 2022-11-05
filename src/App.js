313d5e3afae65b86c53912c3072cbacbdaf2427e
armod <armods@gmail.com>
2022-11-05 20:03:56 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import { Error, Landing, Dashboard, Register } from './pages'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { Stats, Profile, AddJob, AllJob, SharedLayout } from './Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='all-job' element={<AllJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path='landing' element={<Landing />} />
         <Route path='register' element={<Register />} />
         <Route path='*' element={<Error />} />
---
+++ src/Dashboard/index.js
@@ -0,0 +1,7 @@
+import Stats from './Stats'
+import Profile from './Profile'
+import AddJob from './AddJob'
+import AllJob from './AllJob'
+import SharedLayout from './SharedLayout'
+
+export { Stats, Profile, AddJob, AllJob, SharedLayout }
