f7936809913bfe5c5f36e448012c92208c73c94d
armod <armods@gmail.com>
2022-11-06 22:46:31 +0100

    update

---
+++ src/components/JobsContainer.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
+import { useSelector, useDispatch } from 'react-redux'
+import { json } from 'react-router-dom'
+import Loading from './Loading'
+import { getAllJobs } from '../features/allJobs/allJobsSlice'
+
+const JobsContainer = () => {
+  const { jobs, isLoading } = useSelector((store) => store.allJobs)
+  const dispatch = useDispatch()
+
+  if (isLoading) {
+    return <Loading center />
+  }
+
+  if (json.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    )
+  }
+
+  useEffect(() => {
+    dispatch(getAllJobs())
+  }, [])
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />
+        })}
+      </div>
+    </Wrapper>
+  )
+}
+
+export default JobsContainer
