36af5d84852f088dbeef7c48311fa4efe5efbed8
armod <armods@gmail.com>
2022-11-07 12:15:57 +0100

    update

--- src/pages/Dashboard/AddJob.js
+++ src/pages/Dashboard/AddJob.js
@@ -8,6 +8,7 @@ import {
   handleChange,
   clearValues,
   createJob,
+  editJob,
 } from '../../features/job/jobSlice'
 
 const AddJob = () => {
@@ -31,6 +32,21 @@ const AddJob = () => {
     e.preventDefault()
     if (!position || !company || !jobLocation) {
       toast.error('Please Fill Out All Fileds')
+      return
+    }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: {
+            position,
+            company,
+            jobLocation,
+            jobType,
+            status,
+          },
+        })
+      )
       return
     }
     dispatch(createJob({ position, company, jobLocation, jobType, status }))
